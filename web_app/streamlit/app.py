dbcbc66de116575ba347f73cd4988d12e0d9ab96
LukaPecoraro <lp3489@student.uni-lj.si>
2024-05-14 02:10:50 +0200

    Interactive map

--- web_app/streamlit/app.py
+++ web_app/streamlit/app.py
@@ -11,16 +11,15 @@ st.set_page_config(layout="wide")
 
 year_list = [2019, 2020, 2021, 2022, 2023, 2024]
 with st.sidebar:
-    st.title('☀️žARKO dashboard')
+    st.title('Pregled')
     selected_year = st.selectbox('Leto', year_list)
 
 st.title('☀️ žARKO') 
-st.write("dwadawdwa")
 
 map = folium.Map(location=[46.0569, 14.85058], zoom_start=8.4, tiles='CartoDB positron')
 geojson_slo = json.load(open('UE.geojson', encoding='utf-8'))
 df = pd.read_csv("UE.csv", encoding='utf-8')
-df["value"] = np.random.randint(1, 100, len(df))
+df["value"] = np.arange(1, len(df) + 1)
 
 choropleth = folium.Choropleth(
     geo_data=geojson_slo,
@@ -33,29 +32,40 @@ choropleth = folium.Choropleth(
 )
 
 for feature in choropleth.geojson.data['features']:
-    feature['properties']['weekly'] = f"Tedensko obsevanje {np.random.randint(1, 100)} W/m2"
+    feature['properties']['weekly'] = f"Tedensko obsevanje {5} W/m2"
 
 col1, col2 = st.columns([2, 1])
+
+
+
 with col1:
-    with st.container():
-        choropleth.geojson.add_child(
-            folium.features.GeoJsonTooltip(['UE_UIME', "weekly"], labels=False)
-        )
-        choropleth.geojson.add_to(map)
-        st_map = st_folium(map, width=700, height=450, returned_objects=[])
+    choropleth.geojson.add_child(
+        folium.features.GeoJsonTooltip(['UE_UIME', "weekly"], labels=False)
+    )
+    choropleth.geojson.add_to(map)
+    #Add marker (click on map an alert will display with latlng values)
 
-    df_obs = df[['UE_UIME', 'value']]
+    st_map = st_folium(map, width=700, height=450)
 
-    df_obs_sorted = df_obs.sort_values(by="value", ascending=False)
+    loc_name = ''
+    if st_map['last_active_drawing']:
+        loc_name = st_map['last_active_drawing']['properties']['UE_UIME']
+
+    st.write(loc_name)
+
+
 
 with col2:
     st.markdown('#### Sončne lokacije ')
+    df_obs = df[['UE_UIME', 'value']]
+    df_obs_sorted = df_obs.sort_values(by="value", ascending=False)
 
-    st.dataframe(df_obs_sorted,
-                column_order=("UE_UIME", "value"),
-                hide_index=True,
-                width=None,
-                column_config={
+    df_copy = df_obs_sorted.copy()
+
+    edited_df = st.data_editor(
+        df_copy,
+        hide_index=True,
+        column_config={
                 "UE_UIME": st.column_config.TextColumn(
                     "UE_UIME",
                 ),
@@ -63,6 +73,8 @@ with col2:
                     "Obsevanje (W/m2)",
                     format="%f",
                     min_value=0,
-                    max_value= 150,# max(df_obs_sorted["value"]),
-                    )}
-                )
+                    max_value= 100,# max(df_obs_sorted["value"]),
+                    ),
+                },
+        disabled=df.columns,
+    )
